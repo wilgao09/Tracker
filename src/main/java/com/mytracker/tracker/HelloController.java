f974e477fdfa9096412de8a04b038ad947edf3ff
wilgao09 <william.gao09@gmail.com>
2023-01-08 00:54:43 -0500

    added redirection on /

--- src/main/java/com/mytracker/tracker/HelloController.java
+++ src/main/java/com/mytracker/tracker/HelloController.java
@@ -2,20 +2,22 @@ package com.mytracker.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class HelloController {
 
+	// @GetMapping("/")
+	// public String index() {
+	// System.out.println("HIT HELLO");
+	// return "Greetings from Spring Boot!";
+	// }
+
 	@GetMapping("/")
-	public String index() {
-		System.out.println("HIT HELLO");
-		return "Greetings from Spring Boot!";
+	public RedirectView redirectWithUsingRedirectView() {
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl("https://next-website-wilgao09.vercel.app");
+		return redirectView;
 	}
-
-	    @GetMapping("/login")
-    public String redirectTo(){
-        return "https://williamgao09.com/";
-    }
-
 }
